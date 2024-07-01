67ad5019e80ee32b1b2bb63a592f30f2b9212201
neeraj <neerajrawat78200@gmail.com>
2024-07-01 20:50:53 +0530

    enable cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,8 +2,15 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:3000'],
+      headers: ['Content-Type', 'Authorization', 'Access-Control-Allow-Headers'],
+    },
+  },
   'strapi::query',
   'strapi::body',
   'strapi::session',
